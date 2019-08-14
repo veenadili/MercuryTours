f65a8c9d5fd6cd9c2900d7b32f9129055a81d1fd
veenadili <veenadili@gmail.com>
2019-08-14 20:22:03 +1000

    Mercury Tour Flight Reservation Test

---
+++ Automate MercuryTours/MercuryTourPOM/reserveFlights.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Threading;
+
+namespace Automate_MercuryTours.MercuryTourPOM
+{
+    public class ReserveFlights
+    {
+        private IWebDriver driver;
+
+        public ReserveFlights(IWebDriver driver)
+        {
+            this.driver = driver;
+            //Initialise all the elements on the reserveFlights Object Model
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Name, Using = "reserveFlights")]
+        private IWebElement reserveflightbtn; //To find the continue button to go to next page
+
+        public BookFlight clickonreserveflightbtn() //Method to go to next Page 
+        {
+            Thread.Sleep(3000);//delay before getting to next page
+            reserveflightbtn.Click();            
+            return new BookFlight(driver); // Returns an instance of the class BookFlight 
+        }
+    }
+}
